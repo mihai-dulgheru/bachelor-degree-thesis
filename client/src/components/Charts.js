8d895ec8969057b264b1ef8c689fbb7d03336901
mihai-dulgheru <63505058+mihai-dulgheru@users.noreply.github.com>
2022-06-07 02:03:42 +0300

    Added CustomPieChart

--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import swal from 'sweetalert'
 import CustomAppBar from './CustomAppBar'
+import CustomPieChart from './CustomPieChart'
 
 const Charts = () => {
   const [user, setUser] = useState({})
+  const [devices, setDevices] = useState([])
   const navigate = useNavigate()
 
   const getUser = async () => {
@@ -37,13 +39,124 @@ const Charts = () => {
     }
   }
 
+  const getDevices = async () => {
+    const response = await fetch('/api/auth/user/devices', {
+      method: 'GET',
+      headers: {
+        authorization: localStorage.getItem('accessToken')
+      }
+    })
+    const data = await response.json()
+    if (data.status === 'ok') {
+      setDevices(data.devices)
+    } else {
+      swal({
+        title: 'Failed',
+        text:
+          data.message[0] >= 'a' && data.message[0] <= 'z'
+            ? data.message[0].toLocaleUpperCase() + data.message.substring(1)
+            : data.message,
+        icon: 'error',
+        button: {
+          text: 'OK',
+          value: true,
+          visible: true,
+          closeModal: true
+        }
+      }).then(() => {
+        navigate('/login')
+      })
+    }
+  }
+
   useEffect(() => {
     getUser()
+    getDevices()
   }, [])
+
+  const getEstimatedConsumption = (device) => {
+    /**
+     * Dishwasher: kWh/100 cycles * 2.08 = kWh/annum
+     * Washing machines and washer dryers: kWh/100 cycles * 2.2 = kWh/annum
+     * Ovens: kWh/cycle * 286 = kWh/annum
+     *
+     * kWh/1000h = kWh * 1000h
+     * 1 year = 365.242199 days
+     * 1 day = 24h
+     * kWh/annum = kWh * year * day
+     *
+     * Energy (kWh) = (W * hrs) / 1000
+     * Energy (kWh) = kW * hrs
+     */
+    const year = 365.242199
+    const day = 24
+    let energy = 0
+    switch (device.unitMeasurement) {
+      case 'W':
+        energy = (device.energyConsumption * device.noOperatingHours) / 1000
+        break
+      case 'kW':
+        energy = device.energyConsumption * device.noOperatingHours
+        break
+      case 'kWh/100 cycles':
+        let kWhAnnum = 0
+        if (device.category === 'Maşini de spălat vase') {
+          kWhAnnum = device.energyConsumption * 2.08
+        } else {
+          kWhAnnum = device.energyConsumption * 2.2
+        }
+        energy = kWhAnnum / (year * day)
+        break
+      case 'kWh/1000h':
+        energy = device.energyConsumption / 1000
+        break
+      case 'kWh/annum':
+        energy = device.energyConsumption / (year * day)
+        break
+      case 'kWh/cycle':
+        energy = (device.energyConsumption * 286) / (year * day)
+        break
+
+      default:
+        break
+    }
+
+    return energy * year * day
+  }
+
+  const getCategory = (device) => {
+    return device.category
+  }
+
+  const getEstimatedConsumptionByCategory = (devices) => {
+    const tempDevices = devices.map((device) => {
+      return {
+        estimatedConsumption: getEstimatedConsumption(device),
+        category: getCategory(device)
+      }
+    })
+    const object = {}
+    for (const device of tempDevices) {
+      if (Object.hasOwnProperty.call(object, device.category)) {
+        object[device.category] += device.estimatedConsumption
+      } else {
+        object[device.category] = device.estimatedConsumption
+      }
+    }
+    const estimatedConsumptionByCategory = []
+    for (const [key, value] of Object.entries(object)) {
+      estimatedConsumptionByCategory.push({
+        name: key,
+        value: value
+      })
+    }
+    return estimatedConsumptionByCategory
+  }
 
   return (
     <div>
       <CustomAppBar user={user} selectedAppBarItem={'Charts'} />
+      <CustomPieChart data={getEstimatedConsumptionByCategory(devices)} title={'Estimated consumption by categories'} />
     </div>
   )
 }
---
+++ client/src/components/CustomPieChart.js
@@ -0,0 +1,76 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import { Cell, Pie, PieChart } from 'recharts'
+import './CustomPieChart.css'
+
+const RADIAN = Math.PI / 180
+const COLORS_3 = ['#EC6B56', '#FFC154', '#47B39C']
+const COLORS_8 = ['#003f5c', '#2f4b7c', '#665191', '#a05195', '#d45087', '#f95d6a', '#ff7c43', '#ffa600']
+
+const getRandomColor = () => {
+  return `#${Math.floor(Math.random() * 0xffffff)
+    .toString(16)
+    .padStart(6, 0)
+    .toUpperCase()}`
+}
+
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5
+  const x = cx + radius * Math.cos(-midAngle * RADIAN)
+  const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+  return (
+    <text x={x} y={y} fill='white' textAnchor={x > cx ? 'start' : 'end'} dominantBaseline='central'>
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  )
+}
+
+const CustomPieChart = ({ data, title }) => {
+  let COLORS = []
+  if (data.length < 4) {
+    COLORS = [...COLORS_3]
+  } else if (data.length < 9) {
+    COLORS = [...COLORS_8]
+  } else {
+    COLORS = data.map(() => getRandomColor())
+  }
+
+  return (
+    <>
+      <Typography variant='h5' className='w-50 text-align-center mt-4'>
+        {title}
+      </Typography>
+      <div className='display-flex w-50 justify-content-center'>
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx='50%'
+            cy='50%'
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={160}
+            fill='#8884d8'
+            dataKey='value'
+          >
+            {data.map((_entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+        <div className='display-flex flex-direction-column justify-content-center'>
+          {data.map((_entry, index) => (
+            <div className='display-flex align-items-center' key={`legend-${index}`}>
+              <div className='w-4 h-4 br-50 mr-2' style={{ backgroundColor: COLORS[index] }}></div>
+              <Typography className='text-align-left vertical-align-middle' style={{ lineHeight: 2 }}>
+                {data[index].name}
+              </Typography>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CustomPieChart
